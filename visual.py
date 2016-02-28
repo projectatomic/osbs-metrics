5328934a23c066c38223c1cc17934257508e6e13
Tim Waugh <twaugh@redhat.com>
2016-02-28 23:31:50 +0000

    Use pandas for csv input

--- visual.py
+++ visual.py
@@ -3,48 +3,32 @@ from bokeh.plotting import *
 from bokeh.charts import Histogram, TimeSeries
 from bokeh.models import Span, NumeralTickFormatter, AdaptiveTicker, Range1d
 import numpy as np
+import pandas as pd
 import sys
 
 
-def get_csv(inputfile):
-    data = []
-    with open(inputfile) as fp:
-        for line in fp.readlines():
-            data.append(line.rstrip().split(','))
+def run(metrics_file, concurrent_file):
+    concurrent = pd.read_csv(concurrent_file, parse_dates=['timestamp'])
 
-    Metrics = namedtuple('Metrics', data[0])
-    return data[0], [Metrics(*row) for row in data[1:]]
-
-
-def run(metrics, concurrent):
-    cfields, cdata = get_csv(concurrent)
-
-    fields, data = get_csv(metrics)
-    completion = np.array([row.completion for row in data],
-                          dtype=np.datetime64)
-    throughput = np.array([row.throughput for row in data], dtype='float64')
-    pulp_push = np.array([row.plugin_pulp_push for row in data],
-                         dtype='float64')
-    upload_size_mb = np.array([row.upload_size_mb for row in data],
-                              dtype='float64')
+    metrics = pd.read_csv(metrics_file, parse_dates=['completion'])
 
     window_size = 30
     window = np.ones(window_size)/float(window_size)
-    throughput_avg = np.convolve(throughput, window, 'same')
+    throughput_avg = np.convolve(metrics['throughput'], window, 'same')
 
-    upload_speed = np.divide(upload_size_mb, pulp_push)
-
-    output_file(metrics.replace('.csv', '.html'))
+    output_file(metrics_file.replace('.csv', '.html'))
 
     # hourly throughput
     s1 = figure(width=800, height=350, x_axis_type='datetime',
                 title='hourly throughput')
-    s1.circle(completion, throughput, color='blue', alpha=0.2, size=12,
+    s1.circle(metrics['completion'],
+              metrics['throughput'],
+              color='blue', alpha=0.2, size=12,
               legend='hourly throughput')
-    s1.line(completion, throughput_avg, color='navy', legend='throughput (avg)')
-    max_throughput = np.max(throughput)
-    max_throughput_completion = completion[np.argmax(throughput)]
-    peak = Span(location=np.max(throughput), dimension='width',
+    s1.line(metrics['completion'],
+            throughput_avg,
+            color='navy', legend='throughput (avg)')
+    peak = Span(location=metrics['throughput'].max(), dimension='width',
                 line_color='green', line_dash='dashed', line_width=3)
     s1.renderers.extend([peak])
 
@@ -54,26 +38,23 @@ def run(metrics, concurrent):
     s2.yaxis.axis_label = 'upload size (Mb)'
     s2.xaxis[0].formatter = NumeralTickFormatter(format="00:00:00")
     s2.xaxis[0].ticker = AdaptiveTicker(mantissas=[1,3,6])
-    s2.square(pulp_push, upload_size_mb, color='orange', alpha=0.2, size=12)
+    s2.square(metrics['plugin_pulp_push'],
+              metrics['upload_size_mb'],
+              color='orange', alpha=0.2, size=12)
 
     # concurrent builds
-    concurrent = {
-        'timestamp': np.array([row.timestamp for row in cdata],
-                              dtype='datetime64'),
-        'nbuilds': np.array([row.nbuilds for row in cdata], dtype='int64'),
-    }
     s3 = figure(width=800, height=350, title='concurrent builds',
                 x_axis_type='datetime')
     s3.line(concurrent['timestamp'],
             concurrent['nbuilds'],
             line_color='green')
-    start = Span(location=completion[0].astype('int64') * 1000,
+    start = Span(location=metrics['completion'][0].timestamp() * 1000,
                  dimension='height', name='infra move',
                  line_color='red', line_dash='dashed', line_width=3)
     s3.renderers.extend([start])
 
     label = 'upload size (Mb)'
-    hsize = Histogram({label: upload_size_mb}, values=label,
+    hsize = Histogram({label: metrics['upload_size_mb']}, values=label,
                       bins=10, plot_width=800, plot_height=350)
 
     # running time by plugin
@@ -87,8 +68,7 @@ def run(metrics, concurrent):
         'running': 'Total build time',
     }
     for plugin, label in plugins.items():
-        plugin_data[label] = np.array([getattr(row, plugin)
-                                        for row in data], dtype='float64')
+        plugin_data[label] = metrics[plugin]
 
     hrunn = Histogram(plugin_data, values=plugins['running'],
                       bins=10, plot_width=800, plot_height=350)

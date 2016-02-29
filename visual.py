27a7296e728aabeba59f21fa6d02fafdf5d73ec4
Tim Waugh <twaugh@redhat.com>
2016-02-29 10:21:38 +0000

    Work-around Histogram bug, other improvements

--- visual.py
+++ visual.py
@@ -1,10 +1,20 @@
-from collections import namedtuple
 from bokeh.plotting import *
 from bokeh.charts import Histogram, TimeSeries
 from bokeh.models import Span, NumeralTickFormatter, AdaptiveTicker, Range1d
+from collections import namedtuple
+import datetime
 import numpy as np
 import pandas as pd
 import sys
+
+
+def MyHistogram(data, bins, **kwargs):
+    # Work around bokeh.charts.Histogram bug
+    p = figure(**kwargs)
+    hist, edges = np.histogram(data, density=True, bins=bins)
+    p.quad(top=hist, bottom=0, left=edges[:-1], right=edges[1:],
+           color='#dd2222', line_color='black')
+    return p
 
 
 def run(metrics_file, concurrent_file):
@@ -12,11 +22,16 @@ def run(metrics_file, concurrent_file):
 
     metrics = pd.read_csv(metrics_file, parse_dates=['completion'])
 
+    # Network fix made 2016-02-26 approx 2000 UTC
+    network_fix = metrics['completion'] > datetime.datetime(2016, 2, 26,
+                                                            20, 0)
+
     window_size = 30
     window = np.ones(window_size)/float(window_size)
     throughput_avg = np.convolve(metrics['throughput'], window, 'same')
 
     output_file(metrics_file.replace('.csv', '.html'))
+    charts = []
 
     # hourly throughput
     s1 = figure(width=800, height=350, x_axis_type='datetime',
@@ -31,66 +46,82 @@ def run(metrics_file, concurrent_file):
     peak = Span(location=metrics['throughput'].max(), dimension='width',
                 line_color='green', line_dash='dashed', line_width=3)
     s1.renderers.extend([peak])
+    charts.append(s1)
 
     # upload size / pulp upload time
-    s2 = figure(width=800, height=350, title='upload size vs pulp upload time')
-    s2.xaxis.axis_label = 'Time uploading to pulp'
-    s2.yaxis.axis_label = 'upload size (Mb)'
-    s2.xaxis[0].formatter = NumeralTickFormatter(format="00:00:00")
-    s2.xaxis[0].ticker = AdaptiveTicker(mantissas=[1,3,6])
-    s2.square(metrics['plugin_pulp_push'],
-              metrics['upload_size_mb'],
-              color='orange', alpha=0.2, size=12)
+    s2b = figure(width=800, height=350,
+                 title='upload size vs pulp upload time (to Feb 26)')
+    s2b.xaxis.axis_label = 'Time uploading to pulp'
+    s2b.yaxis.axis_label = 'upload size (Mb)'
+    s2b.xaxis.formatter = NumeralTickFormatter(format="00:00:00")
+    s2b.xaxis.ticker = AdaptiveTicker(mantissas=[1,3,6])
+    s2b.square(metrics['plugin_pulp_push'][~network_fix],
+               metrics['upload_size_mb'][~network_fix],
+               color='orange', alpha=0.2, size=12)
+    charts.append(s2b)
+
+    s2a = figure(width=800, height=350,
+                 title='upload size vs pulp upload time (since Feb 26)')
+    s2a.xaxis.axis_label = 'Time uploading to pulp'
+    s2a.yaxis.axis_label = 'upload size (Mb)'
+    s2a.xaxis.formatter = NumeralTickFormatter(format="00:00:00")
+    s2a.xaxis.ticker = AdaptiveTicker(mantissas=[1,3,6])
+    s2a.square(metrics['plugin_pulp_push'][network_fix],
+               metrics['upload_size_mb'][network_fix],
+               color='orange', alpha=0.2, size=12)
+    charts.append(s2a)
 
     # concurrent builds
     s3 = figure(width=800, height=350, title='concurrent builds',
                 x_axis_type='datetime')
     s3.line(concurrent['timestamp'],
             concurrent['nbuilds'],
-            line_color='green')
+            line_color='green',
+            line_join='bevel')
     start = Span(location=metrics['completion'][0].timestamp() * 1000,
                  dimension='height', name='infra move',
                  line_color='red', line_dash='dashed', line_width=3)
     s3.renderers.extend([start])
+    charts.append(s3)
 
-    label = 'upload size (Mb)'
-    hsize = Histogram({label: metrics['upload_size_mb']}, values=label,
-                      bins=10, plot_width=800, plot_height=350)
+    valid = ~np.isnan(metrics['upload_size_mb'])
+    hsize = MyHistogram(metrics['upload_size_mb'][valid], bins=10,
+                        title='Upload size',
+                        plot_width=800, plot_height=350)
+    hsize.xaxis.axis_label = 'Mb'
+    charts.append(hsize)
 
     # running time by plugin
-    plugin_data = {}
-    plugins = {
-        'plugin_pull_base_image': 'Time pulling base image',
-        'plugin_distgit_fetch_artefacts': 'Time fetching sources',
-        'docker_build': 'Time in docker build',
-        'plugin_squash': 'Time squashing layers',
-        'plugin_pulp_push': 'Time uploading to pulp',
-        'running': 'Total build time',
-    }
-    for plugin, label in plugins.items():
-        plugin_data[label] = metrics[plugin]
+    for values, bins, title in [
+            (metrics[valid]['running'], None,
+             'Total build time'),
 
-    hrunn = Histogram(plugin_data, values=plugins['running'],
-                      bins=10, plot_width=800, plot_height=350)
-    hrunn._xaxis.formatter = NumeralTickFormatter(format="00:00:00")
-    hpull = Histogram(plugin_data, values=plugins['plugin_pull_base_image'],
-                      bins=6, plot_width=800, plot_height=350)
-    hpull._xaxis.formatter = NumeralTickFormatter(format="00:00:00")
-    hfetc = Histogram(plugin_data, values=plugins['plugin_distgit_fetch_artefacts'],
-                      bins=6, plot_width=800, plot_height=350)
-    hfetc._xaxis.formatter = NumeralTickFormatter(format="00:00:00")
-    hbuil = Histogram(plugin_data, values=plugins['docker_build'],
-                      bins=10, plot_width=800, plot_height=350)
-    hbuil._xaxis.formatter = NumeralTickFormatter(format="00:00:00")
-    hsqua = Histogram(plugin_data, values=plugins['plugin_squash'],
-                      bins=10, plot_width=800, plot_height=350)
-    hsqua._xaxis.formatter = NumeralTickFormatter(format="00:00:00")
-    hpulp = Histogram(plugin_data, values=plugins['plugin_pulp_push'],
-                      bins=10, plot_width=800, plot_height=350)
-    hpulp._xaxis.formatter = NumeralTickFormatter(format="00:00:00")
+            (metrics[valid]['plugin_pull_base_image'], 6,
+             'Time pulling base image'),
 
-    p = vplot(s1, s2, s3, hsize, hrunn, hpull, hfetc, hbuil, hsqua, hpulp)
-    
+            (metrics[valid]['plugin_distgit_fetch_artefacts'], 6,
+             'Time fetching sources'),
+
+            (metrics[valid]['docker_build'], None,
+             'Time in docker build'),
+
+            (metrics[valid]['plugin_squash'], None,
+             'Time squashing layers'),
+
+            (metrics[valid]['plugin_pulp_push'][~network_fix], None,
+             'Time uploading to pulp (to Feb 26)'),
+
+            (metrics[valid]['plugin_pulp_push'][network_fix], None,
+             'Time uploading to pulp (since Feb 26)'),
+    ]:
+        h = MyHistogram(values, title=title, x_axis_type='datetime',
+                        bins=bins or 10, plot_width=800, plot_height=350)
+        h.xaxis.formatter = NumeralTickFormatter(format="00:00:00")
+        h.xaxis.ticker = AdaptiveTicker(mantissas=[1,3,6])
+        h.yaxis.bounds = (0, len(metrics[valid]))
+        charts.append(h)
+
+    p = vplot(*charts)
     show(p)
 
 

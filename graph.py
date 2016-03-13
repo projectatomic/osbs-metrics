164e2d2c446c8a5c6d64cd8a2c4a3561972b33e8
Tim Waugh <twaugh@redhat.com>
2016-03-13 14:02:28 +0000

    graph: trim excess tags

--- graph.py
+++ graph.py
@@ -7,6 +7,9 @@ import sys
 class BuildTree(object):
     def __init__(self, builds):
         self.deps = defaultdict(set)
+        self.seen = set()
+        builds.sort(key=lambda x: x['metadata']['creationTimestamp'],
+                    reverse=True)
         for build in builds:
             self.add(build)
 
@@ -21,7 +24,31 @@ class BuildTree(object):
         repos = [strip_registry_from_image(repo)
                  for repos in repositories.values()
                  for repo in repos]
+        duplicates = self.seen.intersection(repos)
+        if duplicates:
+            return
+
+        self.seen.update(repos)
         self.deps[strip_registry_from_image(base_image_name)].update(repos)
+
+    def _trim_layers(self, base):
+        layers = self.deps[base]
+        excess = set()
+        for layer in layers:
+            name, version = layer.split(':', 1)
+            if version == 'latest':
+                pass
+            elif layer not in self.deps:
+                # Leaf node
+                excess.add(layer)
+
+        self.deps[base] = list(set(layers) - excess)
+
+    def trim_excess_tags(self):
+        images = [image for image in self.deps.keys()]
+        for base in images:
+            if base in self.deps:
+                self._trim_layers(base)
 
     def __repr__(self):
         return repr(self.deps)
@@ -32,7 +59,9 @@ def run(inputfile):
         builds = json.load(fp)
 
     from pprint import pprint
-    pprint(BuildTree(builds).deps)
+    tree = BuildTree(builds)
+    tree.trim_excess_tags()
+    pprint(tree.deps)
 
 
 if __name__ == '__main__':
